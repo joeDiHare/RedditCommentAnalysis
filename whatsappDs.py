f524a75dee19ad0574bf0c8e3a15a124fdcfac84
Stefano Cosentino <stefano286@gmail.com>
2016-09-19 23:51:16 +0100

    New features

--- whatsappDs.py
+++ whatsappDs.py
@@ -1,32 +1,31 @@
 import csv
 import re
 import datetime
-import time
-import numpy as np
 import pandas
 from collections import Counter
-from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-from scipy.sparse import csr_matrix
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import stopwords
 import difflib
 from datetime import date, timedelta
+import matplotlib.pyplot as plt
+import string
+import numpy as np
 
 # script to read-in list of words
 filename = "C:/Users/Stefano/Documents/chat.txt"
 MsgErr1 = 'Messages you send to this chat and calls are now secured with end-to-end encryption. Tap for more info.'
+MsgErr2 = '<Media omitted>'
 print("Reading chat conversation & data validation...      ", end="")
-results = []
+results, mediaSender, mediaCaption = [], [], []
 with open(filename, newline='',encoding='UTF8') as inputfile:
     for row in csv.reader(inputfile):
-        if row!=[]:
-            if re.search(r'(\d+/\d+/\d+)', row[0]) is None:
+        if row!=[]:#ignore empty lines
+            if re.search(r'(\d+/\d+/\d+)', row[0]) is None: #if not a new sender, attach to previous
                 results[-1][-1] = results[-1][-1] + ' ' + row[0]
-            elif row[1].partition('-')[-1].partition(':')[0].strip()!= MsgErr1:
-                results.append(row)
+            elif row[1].partition('-')[-1].partition(':')[0].strip()!= MsgErr1: #remove error messages
+                if row[1].partition(':')[-1].partition(':')[-1].strip()[:15]== MsgErr2: #count <Media omitted> and remove
+                    mediaSender.append(row[1].partition('-')[-1].partition(':')[0].strip())
+                    mediaCaption.append(row[1].partition(':')[-1].partition(':')[-1].strip())
+                else:
+                    results.append(row)
 print('[done]')
 
 print("Create lists...      ", end="")
@@ -54,10 +53,18 @@ for u in range(0,len(users)):
         indt.append(True) if n==users[u] else indt.append(False)
     ind.append(indt)
 
-# how many individual messages from users
-letter_counts = Counter(sender)
-df = pandas.DataFrame.from_dict(letter_counts, orient='index')
+# WHO MESSAGED THE MOST?
+message_counts = Counter(sender)
+df = pandas.DataFrame.from_dict(message_counts, orient='index')
 df.plot(kind='bar')
+
+# WHO SENDD MORE MEDIA?
+mediaSender_counts = Counter(mediaSender)
+df2 = pandas.DataFrame.from_dict(mediaSender_counts, orient='index')
+df2.plot(kind='bar')
+
+for u in users:
+    print(u+' sent ' +str(message_counts[u])+' messages and '+str(mediaSender_counts[u])+' images/videos.')
 
 # MOST COMMON 20 WORDS PER USER
 # script to read-in strop words
@@ -65,7 +72,8 @@ filename = 'stopwords.txt'; stopwords = []
 with open(filename, newline='',encoding='UTF8') as inputfile:
     for row in csv.reader(inputfile):
         stopwords.append(row[0].lower())
-NoWrdsUsr, bodyUsr, count =[],[],[]
+NoWrdsUsr, bodyUsr, bodyCompact, count =[],[],[],[]
+punctuation = set(string.punctuation)
 for u in range(0,len(users)):
     bodyUsr.append([body[i] for i, x in enumerate(ind[u]) if x])
     NoWrdsUsr.append(len(''.join([body[i] for i, x in enumerate(ind[u]) if x]).split(' ')))
@@ -73,56 +81,70 @@ for u in range(0,len(users)):
     print('\nTF-IDF Analysis for user: ' + users[u] )
     count.append(Counter(word for word in ' '.join(bodyUsr[u]).lower().split() if word not in stopwords).most_common(20))
     print(count[u])
+    s = ''.join(ch for ch in ' '.join(bodyUsr[u]).lower() if ch not in punctuation)
+    bodyCompact.append(s.split()) #compact version of body, all in one string
+
+
+
 
 # HOW MANY JINX?
 jinxNo = []
 for u in range(0,len(users)):
     jinxNo.append(len(difflib.get_close_matches('jinx', ' '.join(bodyUsr[u]).lower().split(), n=100, cutoff=.8)))
-    print('User: ' + users[u] + ' jinxed ' + str(jinxNo[u]) + ' times.')
+    print(users[u] + ' jinxed ' + str(jinxNo[u]) + ' times.')
+
+# HOW MANY 'LOVE' or 'I LOVE YOU'?
+noLove, noWhy = [],[]; noHateU, noIloveU = [0]*len(users),[0]*len(users)
+for u in range(0,len(users)):
+    noLove.append(bodyCompact[u].count('love'))
+    noWhy.append(bodyCompact[u].count('why'))
+    for i in range(0,len(bodyCompact[u])-3):
+        if bodyCompact[u][i] + ' ' + bodyCompact[u][i + 1] == "love you" \
+                or bodyCompact[u][i] + ' ' + bodyCompact[u][i + 1] == "luv you": #bodyCompact[u][i]+' '+bodyCompact[u][i+1]+' '+bodyCompact[u][i+2] == "i love you" or
+            noIloveU[u] += 1
+        if bodyCompact[u][i] + ' ' + bodyCompact[u][i + 1] == "hate you": #bodyCompact[u][i]+' '+bodyCompact[u][i+1]+' '+bodyCompact[u][i+2] == "i love you" or
+            noHateU[u] += 1
+    print(users[u] + " used the word 'love' " + str(noLove[u]) + " times, and said 'I love you' "+str(noIloveU[u])+" times, but also 'I hate you' "+str(noHateU[u])+" times.")
 
 # 0000000000000000000000000000000000000000000000000000000000000000000000
 # TIME ANALYSIS
-# find unique dates
+# find unique dates with messages
 Duniq=['initialize']
 for n in dates:
     Duniq.append(n) if Duniq[-1]!=n else ''
 Duniq.pop(0)
-# noDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, \
-#           31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-# noMsgPerDay=[]
-# for n in Duniq:
-#     noMsgPerDay.append(dates.count(n))
 
-# a list containing all of the dates
+# a list containing all uniques days of the dates from
 d1 = date(int(dates[0][-4:10]),int(dates[0][3:5]),int(dates[0][0:2]))
 d2 = date(int(dates[-1][-4:10]),int(dates[-1][3:5]),int(dates[-1][0:2]))
 dd = [d1 + timedelta(days=x) for x in range((d2-d1).days + 1)]
 noMsgPerDay=[]
 for d in dd:
     noMsgPerDay.append(dates.count(d.strftime('%d/%m/%Y')))
-    # print(d.strftime('%d/%m/%y'))
+
+from itertools import groupby
+grouped_L = [(k, sum(1 for i in g)) for k,g in groupby(noMsgPerDay)]
+counter=[]
+for n in grouped_L:
+    if n[0]==0:
+        counter.append(n[1])
+c = [n[1] for n in grouped_L if n[0] is 0]
+
 # how many messages per day?
 # df = pandas.DataFrame.from_dict(noMsgPerDay)
 # df.plot(kind='bar')
-import matplotlib.pyplot as plt
+
 plt.figure(figsize=(12, 9))
 ax = plt.subplot(111)
 ax.spines["top"].set_visible(False); ax.spines["right"].set_visible(False)
 ax.get_xaxis().tick_bottom(); ax.get_yaxis().tick_left()
-plt.xticks(fontsize=14);
+plt.xticks(fontsize=14)
 plt.yticks(range(5000, 30001, 5000), fontsize=14)
-plt.xlabel("days", fontsize=16);
+plt.xlabel("days", fontsize=16)
 plt.ylabel("Number of messages", fontsize=16)
 
 df = pandas.DataFrame.from_dict(dict(zip(Duniq,noMsgPerDay)), orient='index')
 df.plot(kind='bar')
 #plt.show()
 
-# Always include your data source(s) and copyright notice! And for your
-# data sources, tell your viewers exactly where the data came from,
-# preferably with a direct link to the data. Just telling your viewers
-# that you used data from the "U.S. Census Bureau" is completely useless:
-# the U.S. Census Bureau provides all kinds of data, so how are your
-# viewers supposed to know which data set you used?
-plt.text(1300, -5000, "Showing something cool about the convos"
-                      "Author: SC", fontsize=10)
+plt.text(1300, -5000, "Showing something cool about the convos" "Author: SC", fontsize=10)
