167bb45dd38857196abe7cb54ec871497e786b5d
Stefano Cosentino <stefano286@gmail.com>
2016-09-20 23:10:41 +0100

    Add files via upload

--- whatsappDs.py
+++ whatsappDs.py
@@ -114,42 +114,60 @@ for n in dates:
     Duniq.append(n) if Duniq[-1]!=n else ''
 Duniq.pop(0)
 
+#Dates for user
+datesUser=[]
+for u in range(0,len(users)):
+    tmp = [dates[n] for n in range(0, len(dates)) if ind[u][n]]
+    datesUser.append(tmp)
+
 # a list containing all uniques days of the dates from
 d1 = date(int(dates[0][-4:10]),int(dates[0][3:5]),int(dates[0][0:2]))
 d2 = date(int(dates[-1][-4:10]),int(dates[-1][3:5]),int(dates[-1][0:2]))
 dd = [d1 + timedelta(days=x) for x in range((d2-d1).days + 1)]
-noMsgPerDay=[]
-for d in dd:
-    noMsgPerDay.append(dates.count(d.strftime('%d/%m/%Y')))
+noMsgPerDay = []
+for u in range(0, len(users)):
+    tmp = []
+    for d in dd:
+        tmp.append(dates.count(d.strftime('%d/%m/%Y')))
+    noMsgPerDay.append(tmp)
 
-# WHAT DAYS DO WE MESSAGE LESS?
+# WHAT DAYS OF THE WEEK DO WE MESSAGE LESS?
 # extract days with 0 messages
-grouped_L = [[k, sum(1 for i in g)] for k,g in groupby(noMsgPerDay)] #sum occurrences of consecutive numbers
-count_noMsg = [grouped_L[n][1] for n in range(0,len(grouped_L)) if grouped_L[n][0] is 0] # only looks at zeros
-ddays = [dd[n].strftime('%a') for n in range(0,len(noMsgPerDay)) if noMsgPerDay[n] is 0]
 week=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+plt.figure(figsize=(10, 9))
+f, ax1 = plt.subplots(2, sharex=True)
+cols = 'byrg'
+ddaysCount = []
+for u in range(0, len(users)):
+    grouped_L = [[k, sum(1 for i in g)] for k,g in groupby(noMsgPerDay[u])] #sum occurrences of consecutive numbers
+    count_noMsg = [grouped_L[n][1] for n in range(0,len(grouped_L)) if grouped_L[n][0] is 0] # only looks at zeros
+    ddays = [dd[n].strftime('%a') for n in range(0,len(noMsgPerDay[u])) if noMsgPerDay[u][n] is 0]
+    ddaysCount.append(OrderedDict((w, ddays.count(w)) for w in week)) #ddaysCount = {w:ddays.count(w) for w in week}
+    # plot
+    ax1[0].bar(range(len(ddaysCount[u])), ddaysCount[u].values(), color=cols[u],  width=.5, bottom= ddaysCount[u].values() if u>0 else [0]*7)
+    plt.xticks(range(len(ddaysCount[u])), ddaysCount[u].keys())
+plt.ylabel('Occurrence'); plt.title('Number of times there were ZERO :( messages on a specific day of the week')
+plt.legend(users)
+
+# WHAT DAYS OF THE WEEK DO WE MESSAGE MORE?
+noMsgPerWeekday=[]
+for u in range(0, len(users)):
+    ddays = [[dd[n].strftime('%a'),noMsgPerDay[u][n]] for n in range(0,len(noMsgPerDay[u])) if noMsgPerDay[u][n] > 0]
+    ddaysCount = OrderedDict((w, 0) for w in week)
+    for d in ddays:
+        ddaysCount[d[0]]=ddaysCount[d[0]]+d[1]
+    noMsgPerWeekday.append(ddaysCount)
+    ax1[1].bar(range(len(noMsgPerWeekday[u])), noMsgPerWeekday[u].values(), color=cols[u], width=.5,bottom=noMsgPerWeekday[u].values() if u > 0 else [0] * 7)
+    plt.xticks(range(len(noMsgPerWeekday[u])), noMsgPerWeekday[u].keys())
+plt.ylabel('Occurrence'); plt.title('Messaging during the week')
+plt.show()
+
+
 ddaysCount = OrderedDict((w, ddays.count(w)) for w in week) #ddaysCount = {w:ddays.count(w) for w in week}
 # plot
 plt.bar(range(len(ddaysCount)), ddaysCount.values(), align='center')
 plt.xticks(range(len(ddaysCount)), ddaysCount.keys())
 plt.show()
-
-# WHAT DAYS DO WE MESSAGE MORE?
-ddays = [[dd[n].strftime('%a'),noMsgPerDay[n]] for n in range(0,len(noMsgPerDay)) if noMsgPerDay[n] > 0]
-ddaysCount = OrderedDict((w, 0) for w in week)
-for d in ddays:
-    ddaysCount[d[0]]=ddaysCount[d[0]]+d[1]
-# plot
-plt.bar(range(len(ddaysCount)), ddaysCount.values(), align='center')
-plt.xticks(range(len(ddaysCount)), ddaysCount.keys())
-plt.show()
-
-ddaysCount = OrderedDict((w, ddays.count(w)) for w in week) #ddaysCount = {w:ddays.count(w) for w in week}
-# plot
-plt.bar(range(len(ddaysCount)), ddaysCount.values(), align='center')
-plt.xticks(range(len(ddaysCount)), ddaysCount.keys())
-plt.show()
-
 
 plt.figure(figsize=(12, 9))
 ax = plt.subplot(111)
@@ -162,6 +180,44 @@ plt.ylabel("Number of messages", fontsize=16)
 
 df = pandas.DataFrame.from_dict(dict(zip(Duniq,noMsgPerDay)), orient='index')
 df.plot(kind='bar')
-#plt.show()
 
-plt.text(1300, -5000, "Showing something cool about the convos" "Author: SC", fontsize=10)
+## PLOT Message Distribution over period
+# data
+for u in range(0,len(users)):
+    xdata = range(0,len(dd))
+    ydata = noMsgPerDay[u]
+    # let us make a simple graph
+    fig = plt.figure(figsize=[15,10])
+    ax = plt.subplot(111)
+    l = ax.fill_between(xdata, ydata, facecolor='', alpha=0.5)
+    ax.fill_between(xdata,  [sum(x) for x in zip([0]*len(dd), ydata)], [sum(x) for x in zip(ydata, ydata)], facecolor='blue', alpha=0.5)
+    ax.fill_between(xdata,  [sum(x) for x in zip([0]*len(dd), ydata)], [sum(x) for x in zip(ydata, ydata)], facecolor='yellow', alpha=0.5)
+
+    l = ax.fill_between(xdata, ydata)
+    # set the basic properties
+    ax.set_xlabel('Day posting');ax.set_ylabel('Number of messages');ax.set_title('Message Distribution')
+    # set the limits
+    ax.set_xlim(0, len(dd))
+    ax.set_ylim(0, max(ydata)+5)
+    # change the fill color, edge color and thickness
+    l.set_facecolors([[.5,.5,.8,.3]])
+    l.set_edgecolors([[0, 0, .5, .3]])
+    l.set_linewidths([.5])
+    # add more ticks
+    ax.set_xticks(range(0,len(dd),30))
+    # remove tick marks
+    ax.xaxis.set_tick_params(size=0)
+    ax.yaxis.set_tick_params(size=0)
+    # change the color of the top and right spines to opaque gray
+    ax.spines['right'].set_color((.8,.8,.8))
+    ax.spines['top'].set_color((.8,.8,.8))
+    # tweak the axis labels
+    xlab = ax.xaxis.get_label()
+    ylab = ax.yaxis.get_label()
+    xlab.set_style('italic')
+    xlab.set_size(10)
+    ylab.set_style('italic')
+    ylab.set_size(10)
+    # tweak the title
+    ttl = ax.title
+    ttl.set_weight('bold')
